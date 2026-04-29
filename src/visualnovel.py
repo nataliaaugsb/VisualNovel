babee6cca1f55251eed53328b4efca2e6ce089e0
Natalias-MacBook-Pro <nia210001@utdallas.edu>
2026-04-29 05:17:51 +0000

    added temoc_happy.bmp and added the image to pygame using pygame.image.load

--- src/visualnovel.py
+++ src/visualnovel.py
@@ -1,29 +1,30 @@
+from PIL import Image
 import pygame
-import random
 import sys
 import os
 
-directory = os.path.dirname(os.path.abspath(__file__))
-img_path = os.path.join(directory, "Assets", "temoc_content.png")
-
-print ("path", img_path)
-print ("exists", os.path.exists(img_path))
-print ("size", os.path.getsize(img_path))
 
 
-def main():
+def main():  
     pygame.init()
     pygame.image.get_extended()
     pygame.display.set_caption("A Day with Temoc")
     fullscreen = False
     resolution = (1920, 1080)
     screen = pygame.display.set_mode (resolution, pygame.RESIZABLE)
+    filename = "temoc_happy.bmp"
+    image = pygame.image.load(filename)
+    width, height = image.get_size()
+    new_width = width//3.5
+    new_height = height//3.5
+    new_image = pygame.transform.smoothscale(image,(new_width ,new_height))
     running = True
     while running:
         screen.fill('Black')
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        screen.blit(new_image, (0,-5))
         pygame.display.flip()
     pygame.quit
     sys.exit()
