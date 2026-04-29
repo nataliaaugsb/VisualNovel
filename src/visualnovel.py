b6a19fc362315713aac955bb6008a09a2ee3e7f8
Natalias-MacBook-Pro <nia210001@utdallas.edu>
2026-04-29 01:14:30 +0000

    added screen using def main():, fullscreen, setting resolution and screen.fill

--- src/visualnovel.py
+++ src/visualnovel.py
@@ -4,5 +4,21 @@ import sys
 
 
 
+def main():
+    pygame.init()
+    pygame.display.set_caption("A Day with Temoc")
+    fullscreen = False
+    resolution = (1920, 1080)
+    screen = pygame.display.set_mode (resolution, pygame.RESIZABLE)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill('Orange')
+        pygame.display.flip()
+    pygame.quit
 
 
+if __name__=="__main__":
+    main()
